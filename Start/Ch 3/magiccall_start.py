32f6963be9a98f7f1e50ebb3aae8154bffab277e
omurilolima <muriloserafimlima@gmail.com>
2024-03-12 18:03:04 +0000

    __call__ magic methods

--- Start/Ch 3/magiccall_start.py
+++ Start/Ch 3/magiccall_start.py
@@ -1,5 +1,5 @@
 # Python Object Oriented Programming by Joe Marini course example
-# Using the __str__ and __repr__ magic methods
+# Using the __call__ magic methods
 
 
 class Book:
@@ -12,10 +12,16 @@ class Book:
     def __str__(self):
         return f"{self.title} by {self.author}, costs {self.price}"
 
-    # TODO: the __call__ method can be used to call the object like a function
-
+    # the __call__ method can be used to call the object like a function
+    def __call__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
-# TODO: call the object as if it were a function
+# call the object as if it were a function
+print(b1)
+b2("Anna Karenina", "Leo Tolstoy", 49.95)
+print(b2)
