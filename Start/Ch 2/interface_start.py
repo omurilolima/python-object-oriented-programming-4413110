3cd9373443fb67b3d86cf4be71a84f16f55df8c3
omurilolima <muriloserafimlima@gmail.com>
2024-03-12 15:12:06 +0000

    Using Abstract Base Classes to implement interfaces

--- Start/Ch 2/interface_start.py
+++ Start/Ch 2/interface_start.py
@@ -12,6 +12,10 @@ class GraphicShape(ABC):
     def calcArea(self):
         pass
 
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
 
 class Circle(GraphicShape):
     def __init__(self, radius):
@@ -20,6 +24,10 @@ class Circle(GraphicShape):
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{'Circle': {str(self.calcArea())}}}"
+
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
