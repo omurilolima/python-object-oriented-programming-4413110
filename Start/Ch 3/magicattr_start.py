12caf0fab47662c41b594347a7dd5b60abcc4ea2
omurilolima <muriloserafimlima@gmail.com>
2024-03-12 17:57:21 +0000

    __getattribute__, __setattr__ and __getattr__ magic methods

--- Start/Ch 3/magicattr_start.py
+++ Start/Ch 3/magicattr_start.py
@@ -1,5 +1,8 @@
 # Python Object Oriented Programming by Joe Marini course example
-# Using the __str__ and __repr__ magic methods
+# Using the __getattribute__, __setattr__ and __getattr__ magic methods
+
+
+from typing import Any
 
 
 class Book:
@@ -15,15 +18,31 @@ class Book:
     def __str__(self):
         return f"{self.title} by {self.author}, costs {self.price}"
 
-    # TODO: __getattribute__ called when an attr is retrieved. Don't
+    # __getattribute__ called when an attr is retrieved. Don't
     # directly access the attr name otherwise a recursive loop is created
+    def __getattribute__(self, name):
+        if name == "price":
+            p = super().__getattribute__("price")
+            d = super().__getattribute__("_discount")
+            return p - (p * d)
+        return super().__getattribute__(name)
 
-    # TODO: __setattr__ called when an attribute value is set. Don't set the attr
+    # __setattr__ called when an attribute value is set. Don't set the attr
     # directly here otherwise a recursive loop causes a crash
+    def __setattr__(self, name, value):
+        if name == "price":
+            if type(value) is not float:
+                raise ValueError("The 'price' attr must be a float")
+        return super().__setattr__(name, value)
 
-    # TODO: __getattr__ called when __getattribute__ lookup fails - you can
+    # __getattr__ called when __getattribute__ lookup fails - you can
     # pretty much generate attributes on the fly with this method
+    def __getattr__(self, name):
+        return name + " is not here!"
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
+
+b1.price = float(40)
+print(b1.random)
