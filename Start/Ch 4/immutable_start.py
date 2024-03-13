0ce8af446576493292b5f2269b6b04df12f1026b
omurilolima <muriloserafimlima@gmail.com>
2024-03-13 12:52:02 +0000

    Creating immutable data classes

--- Start/Ch 4/immutable_start.py
+++ Start/Ch 4/immutable_start.py
@@ -4,16 +4,20 @@
 from dataclasses import dataclass
 
 
-@dataclass()  # TODO: "The "frozen" parameter makes the class immutable
+@dataclass(frozen=True)  # TODO: "The "frozen" parameter makes the class immutable
 class ImmutableClass:
     value1: str = "Value 1"
     value2: int = 0
 
+    def some_func(self, newvalue):
+        self.value2 = newvalue
 
-obj = ImmutableClass()
-print(obj.value1)
+obj = ImmutableClass("Another string", 20)
+print(obj.value1, obj.value2)
 
-# TODO: attempting to change the value of an immutable class throws an exception
+# attempting to change the value of an immutable class throws an exception
+# obj.value1 = "Another string"
+# print(obj.value1)
 
-
-# TODO: even functions within the class can't change anything
+# even functions within the class can't change anything
+obj.some_func(5)
